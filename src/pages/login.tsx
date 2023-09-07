921787001c19f0249366cd252a1e09000e8514b1
anubhav-aryan <aryananubhav08@gmail.com>
2023-09-08 01:48:04 +0530

    feat: backend  connection

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const Signup = () => {
+  const navigate = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    // This code will only run on the client side
+    localStorage.removeItem("token");
+  }, []);
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    if (data.user) {
+      alert("Login Successful!");
+      localStorage.setItem("token", data.user);
+      navigate.push("/");
+    } else if (data.status === "passerror") {
+      alert("Wrong Password Try Again");
+    } else {
+      alert("You need to register first.");
+      navigate.push("/signup");
+    }
+    console.log(data);
+  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-white">
-      <form className="w-1/5">
+      <form className="w-1/5"
+      onSubmit={handleSubmit}
+      >
         <div className="flex items-center justify-center">
           <Image src="/Illustration.svg" alt="logo" width={200} height={200} />
         </div>
 
-        <div className="text-center font-bold text-3xl pb-3">Log in to your account</div>
-        <div className="text-center text-gray-500 text-lg">Welcome back! Please enter your details.</div>
+        <div className="text-center font-bold text-3xl pb-3">
+          Log in to your account
+        </div>
+        <div className="text-center text-gray-500 text-lg">
+          Welcome back! Please enter your details.
+        </div>
         <div className="my-6">
           <label
             htmlFor="email"
@@ -24,6 +65,7 @@ const Signup = () => {
             className="shadow-sm  border border-black text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="Enter Your Email"
             required
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="mb-6">
@@ -39,14 +81,15 @@ const Signup = () => {
             className="shadow-sm  border border-black text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="*******"
             required
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        
+
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Sign In 
+          Sign In
         </button>
       </form>
     </div>
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const Signup = () => {
+  const history = useRouter(); // Initialize the history hook
+  const [firstname,setFirstname] = useState('')
+  const [lastname,setLastname] = useState('')
+  const [email,setEmail] = useState('')
+  const [contact,setContact] = useState('')
+  const [password,setPassword] = useState('')
+
+
+  const handleSubmit =  async (e: { preventDefault: () => void; })=>{
+    e.preventDefault()
+    const response = await fetch('http://localhost:5000/api/register',{
+      method : 'POST',
+      headers : {
+        'Content-Type' : 'application/json',
+      },
+      body : JSON.stringify({
+        firstname,
+        lastname,
+        email,
+        contact,
+        password,
+      })
+    })
+    const data = await response.json()
+    if(data.status==='ok'){
+      alert('You have registered successfully and can login now')
+      history.push('/login')
+    }else{
+      alert('You are already registered. Please login')
+      history.push('/login')
+    }
+    console.log(data)
+
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-white">
-      <form className="w-1/5">
+      <form className="w-1/5" onSubmit={handleSubmit}>
         <div className="flex items-center justify-center">
           <Image src="/Illustration.svg" alt="logo" width={200} height={200} />
         </div>
 
         <div className="text-center font-bold text-3xl pb-3">Sign Up</div>
-        <div className="text-center text-gray-500 text-lg">Lets get started</div>
+        <div className="text-center text-gray-500 text-lg">
+          Lets get started
+        </div>
         <div className="mb-6">
           <label
             htmlFor="email"
@@ -24,6 +62,7 @@ const Signup = () => {
             className="shadow-sm  border border-black text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="Enter Your Email"
             required
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="mb-6">
@@ -39,6 +78,7 @@ const Signup = () => {
             className="shadow-sm  border border-black text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="*******"
             required
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="mb-6">
