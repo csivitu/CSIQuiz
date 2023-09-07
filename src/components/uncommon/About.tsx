59a7afb464f19fcc3f7134b6b602f316caab1844
anubhav-aryan <aryananubhav08@gmail.com>
2023-09-07 16:46:02 +0530

    feat: about

--- src/components/uncommon/About.tsx
+++ src/components/uncommon/About.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div>
-      About
+      <section className="">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
+            <img
+              className="object-cover object-center rounded"
+              alt="hero"
+              src="/aboutquiz.png"
+            />
+          </div>
+          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-center md:text-center items-center text-center">
+              <h1 className="font-bold sm:text-4xl text-3xl mb-4 text-[#FED766]">
+                About the Quiz
+              </h1>
+              <p className="mb-8 leading-relaxed text-white font-medium">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-center md:text-center items-center text-center">
+            <h1 className="font-bold sm:text-4xl text-3xl mb-4 text-[#FED766]">
+              About CSI
+            </h1>
+            <p className="mb-8 leading-relaxed text-white font-medium">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
